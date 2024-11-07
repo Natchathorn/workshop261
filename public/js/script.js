49b426ecd21470fab925b6a4b00d077ec5533fe1
Natchathorn <natchathorn.dan@dome.tu.ac.th>
2024-11-07 13:43:43 +0700

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,42 @@
 function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
-    fetch('/api/auth', {
+  
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU077c7e55d9566f5e5ff8d23712d81b3b63f45eb1a7003c7a669b6cdb508882dacc89925e84d8d6855fb09804b90339ac' 
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ "UserName": username, "PassWord": password })
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('message').innerText = data.message;
+        const popupMessage = document.getElementById('message'); // ใช้ 'message' เป็น element แสดงผลด้านล่างปุ่ม
+        if (data.status) {
+            // แสดงชื่อและคณะที่ดึงมาจาก API
+            popupMessage.innerHTML = `
+                <strong>ชื่อ:</strong> ${data.displayname_th}<br>
+                <strong>คณะ:</strong> ${data.faculty}
+            `;
+            popupMessage.style.display = 'block';
+
+        } else {
+            popupMessage.innerText = "Login failed. Please try again.";
+            popupMessage.style.display = 'block';
+
+        }
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+        const popupMessage = document.getElementById('message');
+        popupMessage.innerText = "An error occurred. Please try again.";
+        popupMessage.style.display = 'block';
+        popupMessage.style.color = 'red';
+    });
 }
 
-
-
-function call_REST_API_Hello() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const url = (
-        'http://localhost:8080/hello?' +
-        new URLSearchParams({ myName: username, lastName: password}).toString()
-      );
-    
-    fetch(url)
-    .then(data => {
-        document.getElementById('message').innerText = data.message;
-    })
-    .catch(error => console.error('Error:', error));
+function sHowpass() {
+    const passwordInput = document.getElementById("password");
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 }
